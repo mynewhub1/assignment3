633db2418707ee7c7d38c94379357626684d5404
Alle Dean <allen7242@gmail.com>
2022-07-13 10:45:19 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Travel Form</h1>
+  <img src="https://thumbs.dreamstime.com/z/variant-air-ticket-isolated-white-vector-illustration-44051182.jpg" alt="Image of Airline Tickets">
+
+  <a href="https://vitejs.dev/guide/features.html" target="_blank"></a>
+`
